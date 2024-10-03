fb02e9233e1e9fc510cb8730a2e48caf02d37c01
Arthur Pereira <139659658+Artereira@users.noreply.github.com>
2024-10-03 20:27:48 -0300

    Update index-script.js

--- static/js/index/index-script.js
+++ static/js/index/index-script.js
@@ -43,7 +43,7 @@ category_contents.forEach(category_content => {
         let button = document.createElement("button");
         let strong = document.createElement("strong");
         let button_div = document.createElement("div");
-        button_div.style.backgroundImage = "url(/media/imagens/acoes/Comer/Comer.png)";
+        button_div.style.backgroundImage = "url(/incluiza/media/imagens/acoes/Comer/Comer.png)";
         strong.innerHTML = "Comer";
         
         button.appendChild(button_div);
