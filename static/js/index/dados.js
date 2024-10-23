27200a82edb62951310df30b457db81f9a5399f1
Arthur Pereira <139659658+Artereira@users.noreply.github.com>
2024-10-23 10:40:19 -0300

    Update dados.js

--- static/js/index/dados.js
+++ static/js/index/dados.js
@@ -2,53 +2,53 @@ export var dados = {
     "comida": [
         {
             "name_display": "Ovo",
-            "photo_url": "/static/media/imagens/comida/secoes/comida/ovo/ovo.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/comida/ovo/ovo.png"
         },
         {
             "name_display": "Frango",
-            "photo_url": "/static/media/imagens/comida/secoes/comida/frango/frango.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/comida/frango/frango.png"
         },
         {
             "name_display": "Bife",
-            "photo_url": "/static/media/imagens/comida/secoes/comida/bife/bife.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/comida/bife/bife.png"
         },
     
         {
             "name_display": "Pizza",
-            "photo_url": "/static/media/imagens/comida/secoes/lanche/pizza/pizza.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/lanche/pizza/pizza.png"
         },
         {
             "name_display": "Pão",
-            "photo_url": "/static/media/imagens/comida/secoes/lanche/pao/pao.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/lanche/pao/pao.png"
         },
         {
             "name_display": "Achocolatado",
-            "photo_url": "/static/media/imagens/comida/secoes/lanche/chocolate_na_caneca/cafe.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/lanche/chocolate_na_caneca/cafe.png"
         },
         {
             "name_display": "Marcarrão",
-            "photo_url": "/static/media/imagens/comida/secoes/comida/macarrao/macarrao.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/comida/macarrao/macarrao.png"
         },
         {
             "name_display": "Arroz",
-            "photo_url": "/static/media/imagens/comida/secoes/comida/arroz/arroz.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/comida/arroz/arroz.png"
         },
 
         {
             "name_display": "Maçã",
-            "photo_url": "/static/media/imagens/comida/secoes/frutas/maca/maca.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/frutas/maca/maca.png"
         },
         {
             "name_display": "Banana",
-            "photo_url": "/static/media/imagens/comida/secoes/frutas/banana/banana.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/frutas/banana/banana.png"
         },
         {
             "name_display": "Melancia",
-            "photo_url": "/static/media/imagens/comida/secoes/frutas/melancia/melancia.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/frutas/melancia/melancia.png"
         },
         {
             "name_display": "Pera",
-            "photo_url": "/static/media/imagens/comida/secoes/frutas/pera/pera.png"
+            "photo_url": "/incluiza/static/media/imagens/comida/secoes/frutas/pera/pera.png"
         },
     ],
 
@@ -59,19 +59,19 @@ export var dados = {
     "higene": [
         {
             "name_display": "Chuveiro",
-            "photo_url": "/static/media/imagens/banheiro/chuveiro.png"
+            "photo_url": "/incluiza/static/media/imagens/banheiro/chuveiro.png"
         },
         {
             "name_display": "Escova de dente",
-            "photo_url": "/static/media/imagens/banheiro/escova_de_dentes.png"
+            "photo_url": "/incluiza/static/media/imagens/banheiro/escova_de_dentes.png"
         },
         {
             "name_display": "Papel higiênico",
-            "photo_url": "/static/media/imagens/banheiro/papel_higienico.png"
+            "photo_url": "/incluiza/static/media/imagens/banheiro/papel_higienico.png"
         },
         {
             "name_display": "Vaso sanitario",
-            "photo_url": "/static/media/imagens/banheiro/vaso_sanitario.png"
+            "photo_url": "/incluiza/static/media/imagens/banheiro/vaso_sanitario.png"
         }
     ],
 
